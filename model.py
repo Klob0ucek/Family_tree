3fc668f7e369f21385f194d77d96f4dd7898c8b6
Jan Polacek <janik.polacek@gmail.com>
2024-02-17 22:59:46 +0100

    Added DataIO class for Export and Imports to json

--- model.py
+++ model.py
@@ -2,11 +2,22 @@ from graphviz import Graph
 from uuid import uuid4, UUID
 import json
 
+DEFAUL_FILE = "family_tree.json"
+
 class Data:
     def __init__(self, people=[], couples=[]):
         self.people = people
         self.couples = couples
-        self.import_data("family_tree.json")
+        self.io = DataIO(self)
+        self.initial_data()
+        
+    def initial_data(self):
+        self.io.import_data()
+        if not self.people and not self.couples:
+            p, c = test_data()
+            self.people = p
+            self.couples = c
+            return
     
     def draw_family_tree(self):
         family_tree = Graph(comment='Family Tree')
@@ -28,66 +39,14 @@ class Data:
                 family_tree.edge(str(couple.mother.id), str(child.id))
 
         family_tree.render('family_tree', format='png', cleanup=True)
-    
-    def export_data(self, filename="family_tree.json"):
-        data_dict = {
-            "people": [
-                {"id": str(person.id), "name": person.name, "surname": person.surname, "born": person.born, "death": person.death}
-                for person in self.people
-            ],
-            "couples": [
-                {
-                    "mother_id": str(couple.mother.id),
-                    "father_id": str(couple.father.id),
-                    "family_surname": couple.family_surname,
-                    "marriage": couple.marriage,
-                    "children_ids": [str(child.id) for child in couple.children]
-                }
-                for couple in self.couples
-            ]
-        }
 
-        with open(filename, "w") as file:
-            json.dump(data_dict, file, indent=2)
-
-    def import_data(self, filename):
-        try:
-            with open(filename, "r") as file:
-                data_dict = json.load(file)
-        except FileNotFoundError:
-            # init json data
-            p, c = test_data()
-            self.people = p
-            self.couples = c
-            return
-
-        self.people = [
-            Person(
-                name=person["name"],
-                surname=person["surname"],
-                born=person["born"],
-                death=person["death"],
-                id=UUID(person["id"])
-            )
-            for person in data_dict["people"]
-        ]
-
-        self.couples = [
-            Couple(
-                mother=self.find_person_by_id(UUID(couple["mother_id"])),
-                father=self.find_person_by_id(UUID(couple["father_id"])),
-                family_surname=couple["family_surname"],
-                marriage=couple["marriage"],
-                children=[self.find_person_by_id(UUID(child_id)) for child_id in couple["children_ids"]]
-            )
-            for couple in data_dict["couples"]
-        ]
 
     def find_person_by_id(self, person_id):
         for person in self.people:
             if person.id == person_id:
                 return person
         return None
+
 
     def find_person_by_name(self, name):
         for person in self.people:
@@ -105,14 +64,14 @@ class Person:
             self.id = uuid4()
         self.born = born
         self.death = death
-        self.birth_surname = birth_surname #not exported/imported
-        # rodné příjmení
+        self.birth_surname = birth_surname
     
     def get_name(self):
         return f"{self.name} {self.surname} ({self.born})"
     
     def node_data(self):
-        return f'{person.name} {person.surname}\n* {person.born}\n T {person.death}'
+        birth_name = f"(birth_surname)" if self.birth_surname != "" else ""
+        return f'{person.name} {birth_name} {person.surname}\n* {person.born}\n T {person.death}'
 
 class Couple:
     def __init__(self, mother, father, family_surname, marriage="1.1.2000", children=[]):
@@ -121,18 +80,79 @@ class Couple:
         self.family_surname = family_surname
         self.marriage = marriage
         self.children = children
-        # datum svatby
         
+class DataIO:
+    def __init__(self, data):
+        self.data = data
+    
+    def export_data(self, filename=DEFAUL_FILE):
+        data_dict = {
+            "people": [
+                {
+                    "id": str(person.id),
+                    "name": person.name,
+                    "surname": person.surname,
+                    "born": person.born,
+                    "death": person.death,
+                    "birth_name": person.birth_surname}
+                for person in self.data.people
+            ],
+            "couples": [
+                {
+                    "mother_id": str(couple.mother.id),
+                    "father_id": str(couple.father.id),
+                    "family_surname": couple.family_surname,
+                    "marriage": couple.marriage,
+                    "children_ids": [str(child.id) for child in couple.children]
+                }
+                for couple in self.data.couples
+            ]
+        }
+
+        with open(filename, "w") as file:
+            json.dump(data_dict, file, indent=2)
+        
+    
+    def import_data(self, filename=DEFAUL_FILE):
+        try:
+            with open(DEFAUL_FILE, "r") as file:
+                data_dict = json.load(file)
+        except FileNotFoundError:
+            return
+        self.data.people = [
+            Person(
+                name=person["name"],
+                surname=person["surname"],
+                born=person["born"],
+                death=person["death"],
+                id=UUID(person["id"]),
+                birth_surname=person["birth_name"]
+            )
+            for person in data_dict["people"]
+        ]
+
+        self.data.couples = [
+            Couple(
+                mother=self.data.find_person_by_id(UUID(couple["mother_id"])),
+                father=self.data.find_person_by_id(UUID(couple["father_id"])),
+                family_surname=couple["family_surname"],
+                marriage=couple["marriage"],
+                children=[self.data.find_person_by_id(UUID(child_id)) for child_id in couple["children_ids"]]
+            )
+            for couple in data_dict["couples"]
+        ]
+
+
 def test_data():
     grandpa_pol = Person('Stanislav', 'Poláček', born="4.5.1947")
-    grandma_pol = Person('Anežka', 'Poláčková', born="17.2.1949")
-    grandpa = Person('Rostislav', 'Hvízdal', born="12.9.1943")
+    grandma_pol = Person('Anežka', 'Poláčková', born="17.2.1949", birth_surname="Konečná")
+    grandpa_hviz = Person('Rostislav', 'Hvízdal', born="12.9.1943")
     uncle_pol = Person('Lukáš', 'Poláček', born="27.2.1974")
-    aunt_pol = Person('Petra', 'Poláčková', born="14.7.1980")
-    grandma = Person('Marie', 'Hvízdalová', born="30.7.1946")
+    aunt_pol = Person('Petra', 'Poláčková', born="14.7.1980", birth_surname="Fricová")
+    grandma_hviz = Person('Marie', 'Hvízdalová', born="30.7.1946", birth_surname="Sedláčková")
     uncle = Person('Martina', 'Hvízdalová', born="10.7.1976")
     aunt = Person('Rostislav', 'Hvízdal', born="21.3.1969")
-    mom = Person('Marie', 'Poláčková', born="4.5.1971")
+    mom = Person('Marie', 'Poláčková', born="4.5.1971", birth_surname="Hvízdalová")
     dad = Person('Stanislav', 'Poláček', born="4.5.1971")
     you = Person('Jan', 'Poláček', born="29.12.2002")
     sister = Person('Markéta', 'Poláčková', born="25.10.2005")
@@ -141,20 +161,19 @@ def test_data():
     zuzu = Person('Zuzana', 'Poláčková', born="26.9.2005")
 
     couple3 = Couple(mother=grandma_pol, father=grandpa_pol, family_surname="Poláčkovi", children=[dad, uncle_pol])
-    couple1 = Couple(mother=grandma, father=grandpa, family_surname="Hvízdalovi", children=[mom, aunt, uncle])
+    couple1 = Couple(mother=grandma_hviz, father=grandpa_hviz, family_surname="Hvízdalovi", children=[mom, aunt, uncle])
     couple2 = Couple(mother=mom, father=dad, family_surname="Poláčkovi", children=[you, sister, brother])
     couple4 = Couple(mother=aunt_pol, father=uncle_pol, family_surname="Poláčkovi", children=[luke, zuzu])
 
-    people = [grandpa, grandma, aunt, mom, dad, you, sister, brother, grandma_pol, grandpa_pol, uncle, uncle_pol, aunt_pol, luke, zuzu]
+    people = [grandpa_hviz, grandma_hviz, aunt, mom, dad, you, sister, brother, grandma_pol, grandpa_pol, uncle, uncle_pol, aunt_pol, luke, zuzu]
     couples = [couple1, couple2, couple3, couple4]
     return (people, couples)
 
 
 def main():
-    people, couples = test_data()
-    data = Data(people, couples)
+    data = Data()
+    data.io.export_data()
     data.draw_family_tree()
-    data.export_data()
     
 
 if __name__ == '__main__':
--- ui.py
+++ ui.py
@@ -17,26 +17,30 @@ class PersonDialog(simpledialog.Dialog): # TODO - need to be change if Person ch
         tk.Label(master, text="Enter person's name and surname:").grid(row=0, columnspan=2)
         tk.Label(master, text="Name:").grid(row=1)
         tk.Label(master, text="Surname:").grid(row=2)
-        tk.Label(master, text="Born: (dd.mm.yyyy)").grid(row=3)
-        tk.Label(master, text="Death: (dd.mm.yyyy)").grid(row=4)
+        tk.Label(master, text="Birth surname:").grid(row=3)
+        tk.Label(master, text="Born: (dd.mm.yyyy)").grid(row=4)
+        tk.Label(master, text="Death: (dd.mm.yyyy)").grid(row=5)
 
         self.name_entry = tk.Entry(master)
         self.surname_entry = tk.Entry(master)
+        self.birth_surname = tk.Entry(master)
         self.born = tk.Entry(master)
         self.death = tk.Entry(master)
 
         self.name_entry.grid(row=1, column=1)
         self.surname_entry.grid(row=2, column=1)
-        self.born.grid(row=3, column=1)
-        self.death.grid(row=4, column=1)
+        self.birth_surname.grid(row=3, column=1)
+        self.born.grid(row=4, column=1)
+        self.death.grid(row=5, column=1)
 
     def apply(self):
         name = self.name_entry.get()
         surname = self.surname_entry.get()
+        birth_surname = self.birth_surname.get()
         born_date = self.born.get()
         death_date = self.death.get()
         
-        self.result = Person(name, surname, born=born_date, death=death_date)
+        self.result = Person(name, surname, born=born_date, death=death_date, birth_surname=birth_surname)
 
 
 class CoupleDialog(simpledialog.Dialog): # TODO - need to be change if Couple changes
@@ -260,8 +264,7 @@ class FamilyTreeUI:
 
 
 if __name__ == "__main__": # maybe move this to main.py?
-    people, couples = test_data()
-    data = Data(people, couples)
+    data = Data([], [])
     family_tree_ui = FamilyTreeUI(data)
     family_tree_ui.run()
 
